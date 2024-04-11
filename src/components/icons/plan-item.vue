de2172a4a59480d4ef334ede4f1fa9bd2300b9d0
erikmoratrejo <erikmoratrejo@gmail.com>
2024-04-11 12:18:29 -0600

    🌟First Component

---
+++ src/components/icons/plan-item.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+<template>
+   <div class="plan">
+        <div class="description">
+          <span class="title">
+            El Soltero
+         </span>
+     </div>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
